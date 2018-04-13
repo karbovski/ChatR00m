1cf73aa88d39e235bf8baf074b823edfadc17ceb
Ole <olemikole@hotmail.no>
2018-04-13 13:15:49 +0200

    fjernet startActivity og lagt til readint

--- app/src/main/java/com/karbo/myapplication/LoginActivity.java
+++ app/src/main/java/com/karbo/myapplication/LoginActivity.java
@@ -17,7 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     public void joinChatOnClick(View view) {
 
         Intent intent = new Intent(this, MainActivity.class);
-        
+
 
     }
 
